36399305fe9af271df0c728dbb2a0f1f6685d00b
ahmedabbas358 <monstersudan1@gmail.com>
2026-06-30 15:05:36 +0200

    V-Downloader Initial Release - Apple Glassmorphism UI & Max Privacy

---
+++ app/src/main/java/com/junkfood/seal/DownloadService.kt
@@ -0,0 +1,78 @@
+package com.junkfood.seal
+
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Intent
+import android.os.Binder
+import android.os.Build
+import android.os.IBinder
+import android.util.Log
+import com.junkfood.seal.util.NotificationUtil
+import android.net.wifi.WifiManager
+import android.os.PowerManager
+import android.util.Log
+import com.junkfood.seal.util.NotificationUtil
+import com.junkfood.seal.util.NotificationUtil.SERVICE_NOTIFICATION_ID
+
+private const val TAG = "DownloadService"
+
+/** This `Service` does nothing */
+class DownloadService : Service() {
+
+    private var wakeLock: PowerManager.WakeLock? = null
+    private var wifiLock: WifiManager.WifiLock? = null
+
+    override fun onBind(intent: Intent): IBinder {
+        val pendingIntent: PendingIntent =
+            Intent(this, MainActivity::class.java).let { notificationIntent ->
+                PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE)
+            }
+        val notification = NotificationUtil.makeServiceNotification(pendingIntent)
+        startForeground(SERVICE_NOTIFICATION_ID, notification)
+        acquireLocks()
+        return DownloadServiceBinder()
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        Log.d(TAG, "onUnbind: ")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            stopForeground(STOP_FOREGROUND_REMOVE)
+        } else {
+            stopForeground(true)
+        }
+        stopSelf()
+        releaseLocks()
+        return super.onUnbind(intent)
+    }
+
+    private fun acquireLocks() {
+        if (wakeLock == null) {
+            val powerManager = getSystemService(POWER_SERVICE) as PowerManager
+            wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "Seal::DownloadWakeLock")
+        }
+        if (wakeLock?.isHeld == false) {
+            wakeLock?.acquire()
+        }
+
+        if (wifiLock == null) {
+            val wifiManager = applicationContext.getSystemService(WIFI_SERVICE) as WifiManager
+            wifiLock = wifiManager.createWifiLock(WifiManager.WIFI_MODE_FULL_HIGH_PERF, "Seal::DownloadWifiLock")
+        }
+        if (wifiLock?.isHeld == false) {
+            wifiLock?.acquire()
+        }
+    }
+
+    private fun releaseLocks() {
+        wakeLock?.let {
+            if (it.isHeld) it.release()
+        }
+        wifiLock?.let {
+            if (it.isHeld) it.release()
+        }
+    }
+
+    inner class DownloadServiceBinder : Binder() {
+        fun getService(): DownloadService = this@DownloadService
+    }
+}
